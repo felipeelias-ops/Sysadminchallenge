9a7f99914ab0cfa2f2d1b81d09e10ab468193e70
felipeelias-ops <72135659+felipeelias-ops@users.noreply.github.com>
2023-02-10 11:54:55 -0300

    Update script_1.sh

--- script_1.sh
+++ script_1.sh
@@ -6,7 +6,7 @@ n=1
 while read line;
 do
 #access ssh and get info from servers
-value2=$(ssh -T myuser@$line "uname -s -r -v && hostname -I | awk '{print $1}")
+value2=$(ssh -T myuser@$line "uname -s -r -v -i && hostname -I | awk '{print $1}")
 n=$((n+1))
 #write info to new document
 echo "date $(date +'%d/%m/%Y %H:%M:%S:%3N') $line $value2">> servers_versions.txt
